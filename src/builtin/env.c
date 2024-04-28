b46ede7e9551789f93ef970d64ee0c0f377995a8
Leopoldo Vodak <lvodak@fu-r7-p7.s19.be>
2024-04-28 16:43:05 +0200

    utils

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -6,32 +6,33 @@
 /*   By: lvodak <lvodak@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:21:14 by lvodak            #+#    #+#             */
-/*   Updated: 2024/04/28 15:49:15 by lvodak           ###   ########.fr       */
+/*   Updated: 2024/04/28 16:35:30 by lvodak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	update_shell_lvl(t_env	**envp)
+void	update_shell_lvl(t_env	*envp)
 {
 	int	n;
 
 	n = 0;
-	while (*envp)
+	while (envp)
 	{
-		if (!ft_strncmp((*envp)->var, "SHLVL", 5))
+		if (!ft_strncmp((envp)->var, "SHLVL", 5))
 		{
-			n = ft_atoi((*envp)->content);
+			n = ft_atoi((envp)->content);
 			if (n == 999)
-				(*envp)->content = ft_strdup("");
+				(envp)->content = ft_strdup("");
 			else if (n > 999)
-				(*envp)->content = ft_strdup("1");
+				(envp)->content = ft_strdup("1");
 			else if (n < 0)
-				(*envp)->content = ft_strdup("0");
+				(envp)->content = ft_strdup("0");
 			else
-				(*envp)->content = ft_itoa(n + 1);
+				(envp)->content = ft_itoa(n + 1);
 			break ;
 		}
+		envp = envp->next;
 	}
 }
 
